3e569ff9b8c807e93acac5c77d706e3de9989547
Dan McClary <dan.mcclary@gmail.com>
2014-11-21 03:23:50 -0500

    added statistics builder for WHO data

--- flu_statistics/country_chop_who.py
+++ flu_statistics/country_chop_who.py
@@ -5,28 +5,53 @@ import sys
 import cx_Oracle
 import pprint
 import re
+import string
 
 def write_to_db(db, dataset):
-  ctine = """CREATE TABLE IF NOT EXISTS %s (
-	  region VARCHAR(12),
-	  country VARCHAR(300) PRIMARY KEY,
-	  year NUMBER
-	  week NUMBER
-	  value NUMBER)""" % dataset['title']
+  dt = "DROP TABLE %s" % dataset['title']
   cursor = db.cursor()
-  cursor.execute(ctine)
-  cursor.prepare("INSERT INTO %s (region, country, year, week, value) VALUES (:1, :2, :3, :4, :5)" % dataset['title'])
-  cursor.executemany(None, dataset['data'])
-  db.commit()
+  try:
+    cursor.execute(dt)
+  except:
+    pass
+  ctine = """CREATE TABLE %s (
+	  region VARCHAR(12),
+	  country VARCHAR(500),
+	  year NUMBER,
+	  week NUMBER,
+	  measure NUMBER,
+          CONSTRAINT %s_id PRIMARY KEY (country, year, week)
+          )""" % (dataset['title'], dataset['title'])
+  try:
+    cursor.execute(ctine)
+  except Exception as e:
+    print e
+    print "failed to create table", dataset['title']
+    pass
+  for country in dataset['data']:
+    try:
+      cursor.prepare("INSERT INTO %s (region, country, year, week, measure) VALUES (:1, :2, :3, :4, :5)" % dataset['title'])
+      cursor.executemany(None, country)
+      db.commit()
+    except Exception as e:
+      print e
+      print country[0]
 
 
 def make_tablename(d):
   title_string = d.split("|")[0]
   if "specimen" in title_string:
-    title = title_string.split("->")[-1].strip()+"Specimens"
+    title = title_string.split("->")[-1].strip()+"_Specimens"
+    if "processed" in title_string:
+      title += "_proc"
+    else:
+      title += "_recv"
   else:
-    title = title_string.split("->")[-1].strip()+"Infections"
-  return title
+    title = title_string.split("->")[-1].strip()+"_Infections"
+  title = re.sub("\(", "", title)
+  title = re.sub("\)", "", title)
+  title = re.sub(" ","_", title)
+  return title[:25]
 
 def makerows(region_code, data):
   dateprefix = data[0]
@@ -38,7 +63,7 @@ def makerows(region_code, data):
   def row_to_tuple(dates, r):
     t = []
     for i in range(1,len(r)):
-      t.append((region_code, r[0], dates[i-1][0], dates[i-1][1], r[i]))
+      t.append((region_code, r[0], int(dates[i-1][0]), int(dates[i-1][1]), int(re.sub(",","",r[i]))))
     return t
   data = map(lambda x: row_to_tuple(dates, x), data)
   return data
@@ -47,13 +72,14 @@ def parseWHOCountryFile(filename):
   region = re.sub("WHO+", "",filename)
   region = re.sub(".psv", "", region)
   region = re.sub("\+","", region)
+  region = region.split("/")[-1]
   raw = open(filename).readlines()
   big_splits = []
   for i in range(len(raw)):
     if len(raw[i].split("|")) == 2:
       big_splits.append(i)
   datasets = []
-  pp = pprint.PrettyPrinter(indent=4)
+  #pp = pprint.PrettyPrinter(indent=4)
   for i in range(0,len(big_splits),2):
     ds = {}
     ds['title'] = raw[big_splits[i]]
@@ -68,17 +94,46 @@ def parseWHOCountryFile(filename):
     ds['data'] = raw[ds['starts']:ds['ends']]
     ds['data'] = makerows(region, ds['data'])
     datasets.append(ds)
-    pp.pprint(ds)
-  for ds in datasets:
-    print ds['title']
+    #pp.pprint(ds)
   return datasets
+
+def build_view(datasets):
+  view = """CREATE OR REPLACE VIEW flu_statistics AS
+  SELECT a.region, a.country, a.year, a.week, \n"""
+  from_clause = " from \n"
+  where_clause = " where \n"
+  column_creation = ""
+  table_creation = ""
+  join_creation = ""
+  for i in range(len(datasets)):
+    column_creation += "{0}.measure as {1}".format(string.ascii_lowercase[i], datasets[i]['title'])
+    
+    table_creation += "{0} {1}".format(datasets[i]['title'], string.ascii_lowercase[i])
+    
+    if (i < len(datasets)-1):
+      column_creation += ",\n"
+      table_creation += ",\n"
+      join_creation += "{1}.country = {0}.country and {1}.year = {0}.year and {1}.week = {0}.week \n".format(string.ascii_lowercase[i], string.ascii_lowercase[i+1])
+      if (i < len(datasets)-2):
+        join_creation += "and\n"
+    else:
+      column_creation += "\n"
+      table_creation += "\n"
+     
+
+  view += column_creation + from_clause + table_creation + where_clause + join_creation
+  return view
     
 def main(filename):
   db = cx_Oracle.connect('fludb', 'flushot', 'localhost:1521/orcl')
+  print "parsing datasets..."
   datasets = parseWHOCountryFile(filename)
+  print "writing to DB..."
   for dataset in datasets:
     write_to_db(db, dataset)
-  
+  print "creating view..."
+  c = db.cursor()
+  c.execute(build_view(datasets))
 
 if __name__ == "__main__":
   main(sys.argv[1])
--- setup/data_science_bootcamp_setup.sh
+++ setup/data_science_bootcamp_setup.sh
@@ -5,7 +5,7 @@ sudo -E yum remove numpy
 #install the necessary libraries
 sudo -E yum install blas blas-devel lapack lapack-devel
 #pip install all our basic python modules
-sudo -E pip install numpy scipy pandas vincent cx-Oracle SQLAlchemy pandasql
+sudo -E pip install numpy scipy pandas vincent cx-Oracle SQLAlchemy pandasql seaborn
 #upgrade spark to a reasonable version
 sudo -E yum install spark-core spark-master spark-worker spark-history-server spark-python
 #python 2.6 requires ipython 1.x, so we need to git clone
@@ -14,6 +14,7 @@ cd ipython
 git checkout 1.x
 git pull origin 1.x
 sudo -E python setup.py install
+sudo -E pip install pyzmq jinja2 tornado
 #install rvm and rubies
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
