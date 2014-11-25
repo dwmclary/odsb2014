77b2ee2861cab98961428162354a16595befb41f
awallop <altheavailw@gmail.com>
2014-11-25 16:59:23 -0500

    Added ipython notebook documenting flu shot data into the db

--- flu_statistics/flu_shots_to_db.py
+++ flu_statistics/flu_shots_to_db.py
@@ -15,13 +15,7 @@ def jsonify_data(d):
 
 
 def write_to_db(db, data):
-
   cursor = db.cursor()
-  #try:
-  #  cursor.execute(dt)
-  #except:
-  #  pass
-
   try:
       cursor.prepare("INSERT INTO flu_shot_json(doc) VALUES (:1)")
       cursor.executemany(None, map(jsonify_data, data['results']))
@@ -35,7 +29,6 @@ def main():
   print "parsing dataset..."
   url = "http://flu-vaccination-map.hhs.gov/api/v1/states.json"
   data = json.load(urllib2.urlopen(url))
-
   print "writing to DB..."
   write_to_db(db, data)
 
