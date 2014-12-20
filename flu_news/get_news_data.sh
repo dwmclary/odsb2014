a0b19a615be5a49b1380de8e302500efd458f17d
Dan McClary <dan.mcclary@gmail.com>
2014-12-20 16:58:29 -0500

    added splitting to get_news_data

--- flu_news/get_news_data.sh
+++ flu_news/get_news_data.sh
@@ -3,5 +3,9 @@
 mkdir data
 cd data
 wget https://s3.amazonaws.com/orcl-dsb-fludata/wikinews/wikinews.json
+split --lines=10000 wikinews.json wikinews_data
+wget http://mattmahoney.net/dc/text8.zip
+unzip text8.zip
+tr -s '[[:punct:][:space:]]' '\n' < text8 > linewise_text_8
 cd ..
 wget https://s3.amazonaws.com/orcl-dsb-fludata/wikinews/wikinews_builder.py
