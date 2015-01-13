a9d09b413699e6c6edaa8374a7c7be62a633359f
Dan McClary <dan.mcclary@gmail.com>
2015-01-13 15:52:59 -0500

    updated setup script

--- setup/data_science_bootcamp_setup.sh
+++ setup/data_science_bootcamp_setup.sh
@@ -5,7 +5,7 @@ sudo -E yum remove numpy
 #install the necessary libraries
 sudo -E yum install blas blas-devel lapack lapack-devel
 #pip install all our basic python modules
-sudo -E pip install numpy scipy pandas vincent cx-Oracle SQLAlchemy pandasql seaborn beautifulsoup4 requests
+sudo -E pip install numpy scipy pandas vincent cx-Oracle SQLAlchemy pandasql seaborn beautifulsoup4 requests feedparser
 #upgrade spark to a reasonable version
 sudo -E yum install spark-core spark-master spark-worker spark-history-server spark-python
 #python 2.6 requires ipython 1.x, so we need to git clone
@@ -26,3 +26,4 @@ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /home/oracle/.rvm/scripts/rvm
 rvm install jruby
+echo "export SPARK_HOME=/usr/lib/spark" >> ~/.bashrc
