0b3b31a3273ca8e2ea998cad0055950e8e7b6ce3
Dan McClary <dan.mcclary@gmail.com>
2015-01-26 20:37:04 -0500

    removed unused directories

--- economics/get_economic_data.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mkdir data
-cd data
-wget https://s3.amazonaws.com/orcl-dsb-fludata/WorldDevelopmentIndicators/WDI_Data.csv
-wget https://s3.amazonaws.com/orcl-dsb-fludata/WorldDevelopmentIndicators/WDI_Country.csv
--- flu_biology/get_biological_data.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mkdir data
-cd data
-wget ftp://ftp.ncbi.nih.gov/genomes/INFLUENZA/influenza.fna.gz
-cd ..
